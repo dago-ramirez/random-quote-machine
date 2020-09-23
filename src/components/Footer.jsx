04ab91b4278927edf50482cf0f3316b93077c381
Dago R <dagoramirezserna@gmail.com>
2020-09-23 18:03:48 -0500

    Se dan estilos adicionales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const ICON_TWITTER = <FontAwesomeIcon icon={faTwitterSquare} />;
 export default function Footer({textColor, bgColor, handleOnClick}) {
     return (
         <div className="flex items-center justify-between px-5">
-            <a style={textColor} href="http://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote" className="text-5xl text-color">{ICON_TWITTER}</a>
-            <button style={bgColor} className="text-white hover:bg-gray-500 font-bold py-2 px-4 rounded" onClick={handleOnClick} id="new-quote">New quote</button>
+            <a style={textColor} href="http://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote" className="text-5xl text-color transition duration-1000 ease-in-out animate-pulse">{ICON_TWITTER}</a>
+            <button style={bgColor} className="text-white transition duration-1000 ease-in-out font-bold py-2 px-4 rounded animate-pulse" onClick={handleOnClick} id="new-quote">New quote</button>
         </div>
     )
 }
--- src/components/GetQuotes.jsx
+++ src/components/GetQuotes.jsx
@@ -40,10 +40,10 @@ export default function GetQuotes() {
     }
 
     return (
-        <div style={bgColor} className="App h-screen  flex items-center justify-center" id="quote-box">
+        <div style={bgColor} className="App h-screen transition duration-1000 ease-in-out flex items-center justify-center" id="quote-box">
             <div className="w-full md:w-8/12 lg:w-6/12 min-h-96 rounded overflow-hidden bg-gray-100 shadow-lg">
-                <h1 style={textColor} className="text-2xl md:text-3xl text-center font-bold mb-3" id="text">"{quote}"</h1>
-                <p style={textColor} className="text-right pr-5 justify-center text-xl md:text-3xl" id="author">{author}</p>
+                <h1 style={textColor} className="transition-colors duration-1000 ease-in-out text-2xl md:text-3xl text-center font-bold mb-3" id="text">"{quote}"</h1>
+                <p style={textColor} className="transition-colors duration-1000 ease-in-out text-right pr-5 justify-center text-xl md:text-3xl" id="author">{author}</p>
                 <Footer textColor={textColor} bgColor={bgColor} handleOnClick={handleOnClick} />
             </div>
         </div>
