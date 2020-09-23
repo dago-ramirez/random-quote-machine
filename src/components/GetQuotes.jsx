d4e8cc2227339e2cb1a4ed28a46d7de7c3ba7266
Dago R <dagoramirezserna@gmail.com>
2020-09-22 23:04:53 -0500

    Se le dan los primeros estilos al proyecto

--- src/components/GetQuotes.jsx
+++ src/components/GetQuotes.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
-import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
-const ICON_TWITTER = <FontAwesomeIcon icon={faTwitterSquare} className=" hover:bg-gray-700" />;
-const ICON_QUOTE_LEFT = <FontAwesomeIcon icon={faQuoteLeft} />;
-const ICON_QUOTE_RIGHT = <FontAwesomeIcon icon={faQuoteRight} />;
+const ICON_TWITTER = <FontAwesomeIcon  icon={faTwitterSquare} />;
 
+let colors = ['lightseagreen', 'crimson', 'lightsalmon', 'darkorange', 'mediumpurple', 'slate', 'mediumseagreen', 'olivedrab', 'cadetblue', 'dodgerblue', 'teal', 'darkorchid'];
+let hoverColors = ['darkcyan', 'lightcoral', 'tomato', 'orangered', 'rebeccapurple', 'darkslateblue', 'seagreen', 'darkolivegreen', 'steelblue', 'cornflowerblue', 'mediumturquoise', 'purple'];
+// let styles = {
+//     bgColor: {
+//         'backgroundColor': ''
+//     },
+//     textColor: {
+//         'color': ''
+//     }
+// }
+let bgColor = {
+    'backgroundColor': '',
+    'transitionDuration': 2
+}
+
+let hoverColor = {
+    'hvColor': ''
+}
+
+let textColor = {
+    'color': ''
+}
 
 export default function GetQuotes() {
     const [quotes, setQuotes] = useState([]);
     const [quote, setQuote] = useState('');
     const [author, setAuthor] = useState('');
-    const QUOTES_LENGTH = quotes.length;
-
+    const QUOTES_LENGTH = quotes.length;    
 
     useEffect(() => {
         async function getQuotes() {
@@ -26,22 +44,53 @@ export default function GetQuotes() {
         }
 
         getQuotes();
+
+        const index = Math.floor(Math.random() * 12);
+        // styles.bgColor['backgroundColor'] = colors[index];
+        // styles.textColor['color'] = colors[index];
+        bgColor = {
+            'backgroundColor': colors[index],
+            'transitionDuration': 2
+        }
+        hoverColor = {
+            'hvColor': hoverColors[index]
+        }
+        textColor = {
+            'color': colors[index]
+        }
     }, [])
+
+    function colorGenerator() {
+        const index = Math.floor(Math.random() * 12);
+        // styles.bgColor['backgroundColor'] = colors[index];
+        // styles.textColor['color'] = colors[index];
+        bgColor = {
+            'backgroundColor': colors[index],
+            'transitionDuration': 2
+        }
+        hoverColor = {
+            'hvColor': hoverColors[index]
+        }
+        textColor = {
+            'color': colors[index]
+        }
+    }
 
     function handleOnClick() {
         const INDEX = Math.ceil(Math.random() * QUOTES_LENGTH);
         setQuote(quotes[INDEX].text);
         setAuthor(quotes[INDEX].author === null ? 'Anonimous' : quotes[INDEX].author);
+        colorGenerator()
     }
 
     return (
-        <div className="App h-screen bg-blue-400 flex items-center justify-center" id="quote-box">
+        <div style={bgColor} className="App h-screen  flex items-center justify-center" id="quote-box">
             <div className="w-full md:w-8/12 lg:w-6/12 min-h-96 rounded overflow-hidden bg-gray-100 shadow-lg">
-                <h1 className="text-2xl md:text-5xl text-center font-bold mb-3" id="text">{ICON_QUOTE_LEFT}{quote}{ICON_QUOTE_RIGHT}</h1>
-                <p className="text-right pr-5 justify-center text-xl md:text-3xl" id="author">{author}</p>
+                <h1 style={textColor} className="text-2xl md:text-3xl text-center font-bold mb-3" id="text">"{quote}"</h1>
+                <p style={textColor} className="text-right pr-5 justify-center text-xl md:text-3xl" id="author">{author}</p>
                 <div className="flex items-center justify-between px-5">
-                    <a href="http://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote" className="text-5xl text-gray-500">{ICON_TWITTER}</a>
-                    <button className="bg-gray-300 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={handleOnClick} id="new-quote">New quote</button>
+                    <a style={textColor} href="http://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote" className="text-5xl text-color">{ICON_TWITTER}</a>
+                    <button style={bgColor} className="transition-all duration-700 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={handleOnClick} id="new-quote">New quote</button>
                 </div>
             </div>
         </div>
